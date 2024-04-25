54d02442e9e7b224fa326ef757552b131c51ae75
Changjun Yeo <lunaradio24@gmail.com>
2024-04-26 00:34:19 +0900

    add: searching function, fix: organise and categorize as functions

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// TMDB에서 영화 정보 가져오기
+let search_input;
+let search_button;
+let fetched_movies;
+
+// option for fetching movie data using TMDB's open API
 const options = {
   method: "GET",
   headers: {
@@ -7,36 +11,40 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTczMGIyZmQyNTAzYjM2ZWI4ZjFlNzEyYTg3MGJmNyIsInN1YiI6IjY2MjY0YTRkMmUyYjJjMDE4NzY4YmIwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.juGftcAcZYfpUAIkI_0vyyur1hz2VtQez05Th_g5ZnQ",
   },
 };
+// fetching function
+function fetchMovies() {
+  //fetched_movies 목록을 초기화 합니다.
+  fetched_movies = [];
+  fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      response.results.forEach((element) => {
+        // API를 이용해 TMDB에서 fetch로 가져온 영화목록을 "fetched_movies" 배열에 하나씩 차례대로 push
+        fetched_movies.push({
+          id: element.id,
+          poster: element.poster_path,
+          title: element.title,
+          overview: element.overview,
+          vote: element.vote_average,
+        });
+        // 영화카드 생성
+        make_card(
+          element.id,
+          element.poster_path,
+          element.title,
+          element.overview,
+          element.vote_average
+        );
+      });
+    })
+    .catch((err) => console.error(err));
+}
 
-fetch(
-  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => {
-    response.results.forEach((element) => {
-      console.log(element);
-      make_card(
-        element.id,
-        element.poster_path,
-        300, //poster_size
-        element.title,
-        element.overview,
-        element.vote_average
-      );
-    });
-  })
-  .catch((err) => console.error(err));
-
-//Template for making a movie card
-async function make_card(
-  movie_id,
-  poster_img,
-  poster_size,
-  title,
-  overview,
-  rating
-) {
+// Template for making a movie card
+async function make_card(movie_id, poster_img, title, overview, rating) {
   const movie_list = document.getElementById("movie-list");
   const new_card = document.createElement("article");
   const new_imgbox = document.createElement("div", { class: "image-box" });
@@ -53,17 +61,78 @@ async function make_card(
   let movie_rating = movie_card.appendChild(new_rating);
 
   movie_card.classList.add("card");
-  movie_card.setAttribute("id", movie_id);
+  movie_card.setAttribute("id", movie_id); //영화 id를 영화카드(article)의 id로 할당
   movie_imgbox.classList.add("image-box");
   movie_poster.setAttribute(
     "src",
-    "https://image.tmdb.org/t/p/" + "w" + poster_size + "/" + poster_img
+    "https://image.tmdb.org/t/p/" +
+      "w" +
+      "300" /*poster_size*/ +
+      "/" +
+      poster_img
   );
   movie_title.textContent = title;
   movie_overview.textContent = overview;
   movie_rating.textContent = "rating: " + rating;
 
+  // 영화 카드 클릭했을 때의 이벤트 생성
   movie_card.addEventListener("click", (event) => {
+    // 영화 id 알려주는 alert 창 띄우기
     alert("영화 id: " + event.currentTarget.id);
+  });
+}
+
+// 클릭 이벤트 생성
+function addClickEvent() {
+  //검색창과 검색버튼 각각 할당해주기
+  search_input = document.getElementById("search-input");
+  search_button = document.getElementById("search-button");
+  //검색 버튼을 클릭했을 때의 이벤트 생성
+  search_button.addEventListener("click", () => {
+    //검색어(검색창에 입력된 글자)가 non-empty일 때만 실행
+    if (search_input.value !== "") {
+      //검색어의 모든 알파벳을 소문자로 변환
+      const lowered_searching = search_input.value.toLowerCase();
+
+      fetched_movies.forEach((element) => {
+        //영화 제목의 모든 알파벳을 소문자로 변환
+        const lowered_title = element.title.toLowerCase();
+        // 영화 id로 해당 카드 찾아내기
+        const card = document.getElementById(element.id);
+        if (lowered_title.includes(lowered_searching)) {
+          // 검색어가 포함된 영화라면 카드를 보이도록 설정
+          card.style.display = "block";
+        } else {
+          // 검색어가 포함되지 않은 영화라면 카드를 숨기도록 설정
+          card.style.display = "none";
+        }
+      });
+    } else {
+      //모든 영화카드를 보여주고
+      fetched_movies.forEach((element) => {
+        const card = document.getElementById(element.id);
+        card.style.display = "block";
+      });
+      //alert창 띄우기
+      alert("검색어를 입력해주세요.");
+    }
+  });
+}
+
+// 이니셜라이즈
+function init_page() {
+  //fetchMovies 함수를 사용해 TMDB에서 영화 목록을 불러와 웹페이지에 영화카드를 표시합니다.
+  fetchMovies();
+  //클릭 이벤트를 모두 생성해줍니다.
+  addClickEvent();
+}
+
+//문서의 현재 상태가 로딩 중이 아니라면 아래를 바로 실행합니다.
+if (document.readyState !== "loading") {
+  init_page();
+} else {
+  //로딩 중이라면 DOM 컨텐츠 로딩을 모두 완료한 후, 실행합니다.
+  document.addEventListener("DOMContentLoaded", function () {
+    init_page();
   });
 }
