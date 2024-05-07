9f181f483641f971b529e194090b33def2be0752
Changjun Yeo <lunaradio24@gmail.com>
2024-05-07 10:54:09 +0900

    자잘한 수정

--- src/fetch.js
+++ src/fetch.js
@@ -12,7 +12,7 @@ const options = {
 
 const url = "https://api.themoviedb.org/3/movie/";
 
-// 영화 정보 fetch 및 영화카드 생성 함수
+// 영화 정보 fetch 함수
 async function fetchMovies() {
   //await로 fetch 함수가 영화 데이터를 다 불러올 때까지 기다리고 난뒤, then을 통해 Promise의 response를 json형태로 반환하도록 처리합니다.
   const response = await fetch(url + "popular", options).then((response) => response.json());
@@ -22,18 +22,18 @@ async function fetchMovies() {
   return fetchedMovies;
 }
 
-// 영화 정보 fetch 및 영화카드 생성 함수
+// 영화 credits 정보 fetch 함수
 function fetchCredits(fetchedMovies) {
   let credits = [];
   fetchedMovies.forEach(async (movie) => {
-    //await로 fetch 함수가 영화 데이터를 다 불러올 때까지 기다리고 난뒤, then을 통해 Promise의 response를 json형태로 반환하도록 처리합니다.
     const response = await fetch(url + "/" + movie.id + "/credits", options).then((response) => response.json());
     //response의 json 객체 중에 "cast" 라는 key의 value만 저장합니다.
     const casts = response.cast;
     //response의 json 객체 중에 "crew" 라는 key의 value만 저장합니다.
     const crews = response.crew;
+    //crews 배열에 들어있는 객체 중에  "job" 이라는 key의 value가 "Director"인 객체를 찾습니다.
     const director = crews.find((crew) => crew.job === "Director");
-
+    //credits 배열에 해당 영화의 id, cast, director 정보가 들어간 객체를 추가합니다.
     credits.push({
       id: movie.id,
       cast: casts,
